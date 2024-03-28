f96cff2e32e5fa095d5e7f034aa5c225434ef998
Alex_ <90696382+Plai83@users.noreply.github.com>
2024-03-28 18:39:09 +0300

    Update OperationsMenu.java

--- src/OperationsMenu.java
+++ src/OperationsMenu.java
@@ -5,13 +5,13 @@ public class OperationsMenu {
     public String menuPrint(){
         String strOperations = "+-*/q";
         while (true){
-            System.out.println("Выбрите желаемую оперцию");
+            System.out.println("Выбрите желаемую операцию");
             System.out.println("-------");
             System.out.println("'+' Сложение");
             System.out.println("'-' Вычитание");
             System.out.println("'*' Умножение");
             System.out.println("'/' Деление");
-            System.out.println("'Q' Деление");
+            System.out.println("'Q' Выход");
             System.out.println("-------");
             String operation = scanner.next().toLowerCase();
             if (strOperations.contains(operation)){
